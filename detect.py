57833dcb96ca9c96bf777ea15168562ed0bbd55c
Prashant Shrestha <prashant.stha246810@gmail.com>
2022-09-20 08:37:05 +0545

    xss added

--- detect.py
+++ detect.py
@@ -5,11 +5,29 @@ def sql_detect():
     r = requests.get(url, cookies=cookies)
     if "SQL syntax" in r.text:
         print("SQL Injection Detected")
+    else:
+        print("SQL Injection Not Detected")
 
 def detect_xss():
-    url = f"http://localhost/vulnerabilities/xss_r/?name=%3Cscript%3Ealert%281%29%3B%3C%2Fscript%3E "
+    url = f"http://localhost/vulnerabilities/xss_r/?name=%3Ch1%3EHello%3C%2Fh1%3E "
     cookies = {"security":"low", "PHPSESSID":"471qoj0dus8uq4je00d6fkme24", "security":"low"}
     r = requests.get(url, cookies=cookies)
-    print(r.text)
+    if  "<h1>Hello</h1>" in r.text:
+        print("XSS Detected")
+    else:
+        print("XSS Not Detected")
 
-detect_xss()
+def main():
+    print("""
+    1. Detect SQL
+    2. Detect XSS
+    """)
+    detect = input("Select an option: ")
+    if detect == "1":
+        sql_detect()
+    elif detect == "2":
+        detect_xss()
+    else:
+        print("Invalid Option")
+
+main()
